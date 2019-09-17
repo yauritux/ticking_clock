d6de0811f8adeb6735838b916926d05288c70265
Muhammad Yauri Maulana Attamimi <yauritux@gmail.com>
2019-09-17 23:56:12 +0700

    add ticking (i.e. time remained) logics

--- pkg/util/time/ticking.go
+++ pkg/util/time/ticking.go
@@ -1,6 +1,7 @@
 package time
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -10,12 +11,21 @@ type TickingClock struct {
 	Initialized bool
 }
 
-func (tc *TickingClock) GetElapsedTime(hour int) (elapsedTime string) {
+func (tc *TickingClock) GetElapsedTime(hour int) (timeRemained string) {
 	if tc.Initialized == false || tc.duration != hour {
 		tc.duration = hour
 		tc.end = time.Now().Local().Add(time.Hour * time.Duration(hour))
 		tc.Initialized = true
 	}
+
+	remaining := tc.end.Sub(time.Now().Local())
+	time.Sleep(1000 * time.Millisecond)
+	timeRemained = fmt.Sprintf(
+		"%02d:%02d:%02d",
+		int(remaining.Hours()),
+		int(remaining.Minutes())%60,
+		int(remaining.Seconds())%60,
+	)
 
 	return
 }
